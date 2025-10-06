05d44fa4160ce9a1ea50afbc83b6518b3527b48a
Hung Nguyen <hungnb94@gmail.com>
2025-10-06 11:37:10 +0700

    First commit

---
+++ core/model/src/main/java/com/tekome/core/model/Image.kt
@@ -0,0 +1,8 @@
+package com.tekome.core.model
+
+data class Image(
+    val id: String,
+    val createAt: String,
+    val url: String,
+    val updateAt: String,
+)
---
+++ settings.gradle.kts
@@ -0,0 +1,27 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "download-images-flow"
+include(":app")
+include(":core:data")
+include(":core:model")
+include(":core:database")
+include(":core:network")
